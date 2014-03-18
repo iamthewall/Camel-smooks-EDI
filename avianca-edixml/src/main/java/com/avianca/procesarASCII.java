9c975ed21aae641e8aa286b213722d914583546c
aromerob <aromerob@redhat.com>
2014-03-18 15:42:14 -0500

    EDI TO XML Final Version

---
+++ avianca-edixml/src/main/java/com/avianca/procesarASCII.java
@@ -0,0 +1,15 @@
+package com.avianca;
+
+public class procesarASCII {	
+	public String doSomething(String body)
+	{		
+		char caracter = 28;
+		
+		if(body.startsWith(caracter+"UNH") == false)
+		{
+			body = caracter + "UNH" + body;
+		}
+		body = body.concat("UNT");				
+		return body;
+	}
+}
