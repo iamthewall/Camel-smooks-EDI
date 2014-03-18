93e20f6052aaf4b725e53225eb1bd31f6b3fbaa1
aromerob <aromerob@redhat.com>
2014-03-18 15:41:27 -0500

    Final version EDI to CSV. With AMQ and without.

---
+++ avianca-edicsv-final/src/main/java/com/avianca/procesarASCII.java
@@ -0,0 +1,14 @@
+package com.avianca;
+
+public class procesarASCII {	
+	public String doSomething(String body)
+	{		
+		body = body
+				.replaceAll("[^\\p{L}\\p{Nd}]", ",")
+				.replaceAll("UNH+", "")
+				.replaceAll(",UNT", "\r\n")
+				.replaceAll("\r\n,", "\r\n")								
+				.substring(1);
+		return body;
+	}
+}
